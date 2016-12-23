aa1279ab11af7fc12101184e42d8f560d4a79cf9
Phil <pacman@crooks.me.uk>
2016-12-23 08:49:05 +0000

    build changes

---
+++ cordova-android-6.0.0/framework/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/phil/Development/MunroBagger/app/cordova-android-6.0.0/framework/bin/AndroidManifest.xml : \
+/home/phil/Development/MunroBagger/app/cordova-android-6.0.0/framework/AndroidManifest.xml \
---
+++ cordova-android-6.0.0/framework/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/phil/Development/MunroBagger/app/cordova-android-6.0.0/framework/bin/AndroidManifest.xml \
